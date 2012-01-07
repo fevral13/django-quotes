33f9f2785df5e71ee0199b98b78a1f68fabb075a
Serj Zavadsky <fevral13@gmail.com>
2012-01-07 14:51:15 +0200

    setup.py added

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ setup(
     author_email='',
     url='https://github.com/fevral13/django-quotes',
     packages=['quotes', 'quotes.templatetags'],
-    data_files=['quotes/templates/quotes/random_quote.html'],
+    #data_files=['quotes/templates/quotes/random_quote.html'],
     package_data={'quotes': 'quotes/templates/quotes/*.html'},
+    #data_files=[('quotes', ['templates/quotes/*.html',])],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
