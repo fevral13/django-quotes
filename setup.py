481e6d6f6f08132fc595559ac7fc95b8b4da46ce
Serj Zavadsky <fevral13@gmail.com>
2012-01-07 14:03:02 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='django-quotes',
+    version='0.1',
+    description="Quotes application for Django.",
+    long_description=open('README.markdown').read(),
+    author='Dave Merwin',
+    author_email='',
+    url="https://github.com/fevral13/django-quotes',
+    packages=['quotes'],
+    package_dir={'quotes': 'quotes'},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
